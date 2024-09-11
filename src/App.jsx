ac6556dc2f3c75204df967d4a7b5cb5cef1c2d18
Ygor Garofalo <igorgarofalo@gmail.com>
2024-09-11 15:14:28 +0200

    fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { MainComp } from "./components/MainCOmp";
+import { MainComp } from "./components/MainComp";
 
 function App() {
   return (
