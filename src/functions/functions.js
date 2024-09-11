2ee192dd6442dd1f45b6a194d43f1bc3ede5f2c7
Ygor Garofalo <igorgarofalo@gmail.com>
2024-09-11 15:39:18 +0200

    fixed again url

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -10,11 +10,7 @@ export const calculate = (
   api
 ) => {
   return fetch(
-    `https://api.miocodicefiscale.com/calculate?lname=${cognome}
-    &fname=${nome}&gender=${sesso}&city=${luogo}
-    &state=${provincia}&abolished=${false}
-    &day=${giorno}&month=${mese}
-    &year=${anno}&omocodia_level=${1}&access_token=${api}`
+    `https://api.miocodicefiscale.com/calculate?lname=${cognome}&fname=${nome}&gender=${sesso}&city=${luogo}&state=${provincia}&abolished=${false}&day=${giorno}&month=${mese}&year=${anno}&omocodia_level=${1}&access_token=${api}`
   )
     .then((res) => {
       if (res.ok) {
