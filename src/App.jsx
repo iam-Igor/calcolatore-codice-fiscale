69f1c0cf4d38da1e51d1331064533a225fe862b4
Ygor Garofalo <igorgarofalo@gmail.com>
2024-09-11 14:44:35 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { MainComp } from "./components/MainCOmp";
+
+function App() {
+  return (
+    <>
+      <MainComp />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/MainComp.jsx
@@ -0,0 +1,139 @@
+import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
+import comuni from "../assets/comuni.json";
+import { useState } from "react";
+
+const MainComp = () => {
+  const [nome, SetNome] = useState("");
+  const [cognome, SetCognome] = useState("");
+  const [luogo, SetLuogo] = useState("");
+  const [provincia, SetProvincia] = useState(comuni[0].codiceCatastale);
+  const [data, SetData] = useState("");
+  const [sesso, SetSesso] = useState("M");
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log(data);
+  };
+
+  return (
+    <Container fluid className="bg_terziary vh-100">
+      <Row>
+        <Col className="d-flex justify-content-center mt-4">
+          <Card
+            className="py-3 bg_secondary rounded-4 p-3 shadow main-card"
+            style={{ width: "50rem" }}
+          >
+            <Card.Body className="p-1">
+              <Card.Title className="bg_primary p-3 rounded-3">
+                Calcolo codice fiscale
+              </Card.Title>
+              <div className="form-cont">
+                <Form
+                  onSubmit={(e) => {
+                    submitForm(e);
+                  }}
+                >
+                  <Form.Group className="mb-3 d-flex  align-items-center ">
+                    <Form.Label className="me-2 right-label">Nome</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Es. Mario"
+                      className="long-input"
+                      value={nome}
+                      onChange={(e) => {
+                        SetNome(e.target.value);
+                      }}
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3 d-flex align-items-center ">
+                    <Form.Label className="me-2 right-label">
+                      Cognome
+                    </Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Es. Rossi"
+                      className="long-input"
+                      value={cognome}
+                      onChange={(e) => {
+                        SetCognome(e.target.value);
+                      }}
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3 d-flex align-items-center">
+                    <Form.Label className="me-2 right-label">
+                      Luogo di nascita
+                    </Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Es. Milano"
+                      className="long-input"
+                      value={luogo}
+                      onChange={(e) => {
+                        SetLuogo(e.target.value);
+                      }}
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3 d-flex align-items-center  justify-content-between">
+                    <div className="d-flex me-1 align-items-center prov-cont">
+                      <Form.Label className="me-2 right-label prov-lab">
+                        Provincia
+                      </Form.Label>
+                      <Form.Select
+                        className="prov-input"
+                        onChange={(e) => {
+                          SetProvincia(e.target.value);
+                        }}
+                      >
+                        {comuni.map((comune, index) => {
+                          return (
+                            <option key={index} value={comune.codiceCatastale}>
+                              {comune.nome}
+                            </option>
+                          );
+                        })}
+                      </Form.Select>
+                    </div>
+                    <div className="d-flex align-items-center sex-cont">
+                      <Form.Label className="me-2">Sesso</Form.Label>
+                      <Form.Select
+                        className="sex-input"
+                        value={sesso}
+                        onChange={(e) => {
+                          SetSesso(e.target.value);
+                        }}
+                      >
+                        <option value="M">M</option>
+                        <option value="F">F</option>
+                      </Form.Select>
+                    </div>
+                  </Form.Group>
+                  <Form.Group
+                    className="mb-3 d-flex align-items-center  "
+                    controlId="exampleForm.ControlInput1"
+                  >
+                    <Form.Label className="right-label">
+                      Data di nascita
+                    </Form.Label>
+                    <Form.Control
+                      type="date"
+                      className="date-input"
+                      value={data}
+                      onChange={(e) => {
+                        SetData(e.target.value);
+                      }}
+                    />
+                  </Form.Group>
+                  <Button className="shadow btn-success" type="submit">
+                    Calcola
+                  </Button>
+                </Form>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export { MainComp };
